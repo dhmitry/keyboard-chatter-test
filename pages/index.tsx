b311aa4db392eb42652d01e64f3cfce8e425200b
Dmitrijs Savkins <dmitrijs.savkins@gmail.com>
2022-04-09 11:01:43 +0300

    Update tile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 import React from 'react';
 import Keyboard from '../components/Keyboard';
 import Settings from '../components/Settings';
@@ -9,6 +10,10 @@ const Home: NextPage = () => {
 
   return (
     <div className={isDarkMode ? 'dark' : ''}>
+      <Head>
+        <title>Keyboard Chatter Test</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div className="flex min-h-screen flex-col items-center justify-center bg-slate-100 transition-colors duration-300 dark:bg-zinc-800">
         <Settings />
         <Keyboard />
