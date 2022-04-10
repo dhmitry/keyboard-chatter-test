bda49240f1346e9d374518676096ff4ce0647f1d
Dmitrijs Savkins <dmitrijs.savkins@gmail.com>
2022-04-10 20:16:02 +0300

    Adjust tooltip

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -8,19 +8,19 @@ interface TooltipProps {
 
 const Tooltip = ({ children, text }: TooltipProps): JSX.Element => {
   const { getTooltipProps, setTooltipRef, setTriggerRef, visible } =
-    usePopperTooltip({ delayShow: 150, delayHide: 300, interactive: true });
+    usePopperTooltip({ delayShow: 100, delayHide: 300, interactive: true });
 
   return (
     <div>
       <div ref={setTriggerRef}>{children}</div>
-      <div ref={setTooltipRef} {...getTooltipProps()}>
-        <div
-          className={`mt-2 rounded bg-zinc-900 px-2 py-1 text-slate-100 ${
-            visible ? 'opacity-100' : 'opacity-0'
-          } transition-opacity duration-300 ease-in-out dark:bg-slate-600`}
-        >
-          {text}
-        </div>
+      <div
+        ref={setTooltipRef}
+        {...getTooltipProps()}
+        className={`mt-2 rounded bg-zinc-900 px-2 py-1 text-slate-100 ${
+          visible ? 'opacity-100' : 'opacity-0'
+        } transition-opacity duration-300 ease-in-out dark:bg-slate-600`}
+      >
+        {text}
       </div>
     </div>
   );
