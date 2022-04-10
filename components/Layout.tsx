efd36cd128ff1cba473d85452117bc119b8bc72a
Dmitrijs Savkins <dmitrijs.savkins@gmail.com>
2022-04-10 17:39:43 +0300

    Wait for settigns to load before showing content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,17 +9,21 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   const { isDarkMode, animationsClasses } = useSettings();
 
+  const isLoading = useMemo(() => isDarkMode === null, [isDarkMode]);
+
   return (
     <div className={isDarkMode ? 'dark' : ''}>
       <Head>
         <title>Keyboard Chatter Test</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div
-        className={`flex min-h-screen flex-col items-center justify-center bg-slate-100 dark:bg-zinc-800 ${animationsClasses}`}
-      >
-        {children}
-      </div>
+      {isLoading ? null : (
+        <div
+          className={`flex min-h-screen flex-col items-center justify-center bg-slate-100 dark:bg-zinc-800 ${animationsClasses}`}
+        >
+          {children}
+        </div>
+      )}
     </div>
   );
 };
