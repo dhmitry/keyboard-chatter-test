c5c055514ab62a62989c06b57fc184fddf6341e5
Dmitrijs Savkins <dmitrijs.savkins@gmail.com>
2022-07-15 20:26:37 +0300

    Adjust tooltip width

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -9,7 +9,7 @@ const Tooltip = ({ children, text }: TooltipProps): JSX.Element => {
   return (
     <div className="group relative">
       {children}
-      <span className="invisible absolute left-[50%] z-10 mt-2 -ml-16 w-32 rounded bg-zinc-900 px-2 py-1 text-center text-slate-100 opacity-0 transition-opacity ease-in-out group-hover:visible group-hover:opacity-100 dark:bg-slate-600">
+      <span className="invisible absolute left-[50%] z-10 mt-2 -ml-32 w-64 rounded bg-zinc-900 p-2 text-center text-slate-100 opacity-0 transition-opacity ease-in-out group-hover:visible group-hover:opacity-100 dark:bg-slate-600">
         {text}
       </span>
     </div>
